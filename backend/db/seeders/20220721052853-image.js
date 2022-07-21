b1fd05f40892bb2aaf23678e88b93c4182b40152
irelius <kihong711@hotmail.com>
2022-07-20 22:59:58 -0700

    model and migrations phase 1 finished

--- backend/db/seeders/20220721052853-image.js
+++ backend/db/seeders/20220721052853-image.js
@@ -27,54 +27,25 @@ const images = [
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    try {
-      for (let image of images) {
-        const { url, reviewId, spotId } = image
-        let foundId;
+    for (let image of images) {
+      const { url, reviewId, spotId } = image
+      let foundId;
 
-        if(image.reviewId) {
-          foundId = await Review.findByPk(image.reviewId)
-          await Image.create({
-            reviewId: foundId.id,
-            url
-          })
+      if (image.reviewId) {
+        foundId = await Review.findByPk(image.reviewId)
+        await Image.create({
+          reviewId: foundId.id,
+          url
+        })
 
-        }
-        if(image.spotId) {
-          foundId = await Spot.findByPk(image.spotId)
-          await Image.create({
-            spotId: foundId.id,
-            url
-          })
-        }
-
-
-
-        // const { url, imageableId, imageableType } = image
-        // let foundId;
-        // if (image.imageableType === "Review") {
-        //   foundId = await Review.findOne({
-        //     where: {
-        //       id: image.imageableId
-        //     }
-        //   })
-        // }
-        // if (image.imageableType === "Spot") {
-        //   foundId = await Spot.findOne({
-        //     where: {
-        //       id: image.imageableId
-        //     }
-        //   })
-        // }
-        // await Image.create({
-        //   imageableId: foundId.id,
-        //   imageableType,
-        //   url
-        // })
       }
-    }
-    catch (e) {
-      console.log(e);
+      if (image.spotId) {
+        foundId = await Spot.findByPk(image.spotId)
+        await Image.create({
+          spotId: foundId.id,
+          url
+        })
+      }
     }
   },
 
