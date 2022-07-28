a67900aa13e25cd2930b3a7963d50a6b4afdc871
irelius <kihong711@hotmail.com>
2022-07-27 20:40:51 -0700

    boiler plate phase 1

---
+++ backend/routes/index.js
@@ -0,0 +1,19 @@
+// backend/routes/index.js
+const express = require('express');
+const router = express.Router();
+
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+
+// Add a XSRF-TOKEN cookie
+// should not be available in production
+router.get("/api/csrf/restore", (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({
+        'XSRF-Token': csrfToken
+    });
+});
+
+module.exports = router;
